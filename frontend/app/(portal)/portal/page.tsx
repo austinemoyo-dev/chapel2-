e571d7871e5dfa2862b479c1df744a2e263f6515
austinemoyo <augustineakinmoyo@gmail.com>
2026-05-18 14:27:08 +0100

    refactor: remove deprecated and unused utility file

--- frontend/app/(portal)/portal/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-export default function PortalRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/student'); }, [router]);
-  return null;
-}
